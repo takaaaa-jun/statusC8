046dd6be67721f4c6516ce03e4d60861ce67d804
takaaaa-jun <takaaaa-jun@users.noreply.github.com>
2026-04-18 18:09:16 +0900

    settings hot reload

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  webpack: (config) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
