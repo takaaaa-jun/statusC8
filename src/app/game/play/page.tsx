d9c7d2f54fb61002487f082276c035d1355e50be
takaaaa-jun <takaaaa-jun@users.noreply.github.com>
2026-04-24 12:24:26 +0000

    setting page

--- src/app/game/play/page.tsx
+++ src/app/game/play/page.tsx
@@ -1,7 +1,3 @@
-export async function GET() {
-    return Response.json({ message: "GET start ok" });
-}
-
-export async function POST(request: Request) {
-    return Response.json({ message: "POST start ok" });
+export default function PlayPage() {
+    return <div>Play Page</div>;
 }
