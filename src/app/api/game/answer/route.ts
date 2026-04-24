4cb5333e4cc8ccd33879280282e201b54fb2805c
takaaaa-jun <takaaaa-jun@users.noreply.github.com>
2026-04-24 12:17:22 +0000

    setting route

--- src/app/api/game/answer/route.ts
+++ src/app/api/game/answer/route.ts
@@ -1,3 +1,7 @@
+export async function GET() {
+    return Response.json({ message: "GET start ok" });
+}
+
 export async function POST(request: Request) {
-    return Response.json({ message: "ok" });
+    return Response.json({ message: "POST start ok" });
 }
